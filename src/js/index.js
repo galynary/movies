aa13bc1107e3fb09c17c0e30692678ab5c6c4d7c
galynary <107271972+galynary@users.noreply.github.com>
2024-09-13 00:28:57 +0300

    update index js

--- src/js/index.js
+++ src/js/index.js
@@ -1,56 +1,6 @@
-const search = document.querySelector('.js-search');
-const list = document.querySelector('.js-list');
-search.addEventListener('submit', onSearch);
-
-function onSearch(evt) {
-  evt.preventDefault();
-  //елементи форми
-  const { query, days } = evt.currentTarget.elements;
-  // значення елементів форми
-  getWeather(query.value, days.value)
-    .then(data => (list.innerHTML = createMarcup(data.forecast.forecastday)))
-    .catch(err => console.log(err));
-}
-
-// ------------запит на погоду---------------
-function getWeather(city, days) {
-  const API_KEY = 'b8c62e719a6e4ef6972172816241607';
-  const BASE_URL = 'http://api.weatherapi.com/v1';
-
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: city,
-    days: days,
-    lang: 'uk',
-  });
-  return fetch(
-    // `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}&lang="uk"` АБО
-    `${BASE_URL}/forecast.json?${params}`
-  ).then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
-    }
-    return resp.json();
-  });
-}
-
-//-------------рендеримо наш список--------------
-function createMarcup(arr) {
-  return arr
-    .map(
-      ({
-        date,
-        day: {
-          avgtemp_c,
-          condition: { icon, text },
-        },
-      }) => `
-  <li>
-  <img src="${icon}" alt ="${text}">
-  <p>${text}</p>
-  <h2>${date}</h2>
-  <h3>${avgtemp_c}</h3>
-  </li>`
-    )
-    .join(' ');
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const ENDPOINT = 'trending/movie/day';
+const API_KEY = 'e7b77a04616bbe4184f610c6f41bfdf9';
+function getTrending() {
+  fetch(`${BASE_URL}${ENDPOINT}?api_key=${API_KEY}`);
 }
